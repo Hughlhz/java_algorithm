de589db5755d1a02705eb6b7d34e3f4f3891bc1c
Hughlhz <hughes.lhz@gmail.com>
2023-12-04 22:51:27 +0800

    完成摆动序列

--- greedyAlgorithm/src/main/java/Leetcode376.java
+++ greedyAlgorithm/src/main/java/Leetcode376.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Leetcode376 {
+public class Leetcode376 {
+    int [] nums;
+    public int wiggleMaxLength(int[] nums) {
+        this.nums=nums;
+        int count=1;
+        int flag=0;
+        if(nums.length==1) return count;
+        int j=0;
+        while(j<nums.length-1)
+        {
+            if (nums[j] < nums[j+1]) {
+                flag = 1;
+                break;
+            }
+            if(nums[j] > nums[j+1]) {
+                flag = -1; break;
+            }
+            j++;
+
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int temp = findMost(flag, i);
+            flag*=-1;
+            if (i == temp) {
+                return count;
+            } else {
+                count++;
+                i=temp-1;
+            }
+        }
+        return count;
+    }
+
+    public int findMost(int flag,int i) {
+        if(flag==1){
+            while (i < nums.length-1 &&nums[i]<=nums[i+1]) i++;
+            return  i;
+        }
+        else if(flag==-1){
+            while (i < nums.length-1 &&nums[i]>=nums[i+1]) i++;
+            return  i;
+        }
+        return i;
+    }
+
+
+
+
 }
--- greedyAlgorithm/src/test/java/LeetcodeTest.java
+++ greedyAlgorithm/src/test/java/LeetcodeTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LeetcodeTest {
+import org.junit.Test;
+
+public class LeetcodeTest {
+    @Test
+    public void leetcode455Test() {
+        Leetcode455 leetcode = new Leetcode455();
+        int[] g = {1, 2, 3};
+        int[] s = {1, 2};
+        System.out.println(leetcode.findContentChildren(g,s));
+    }
+    @Test
+    public void leetcode376Test() {
+        Leetcode376 leetcode = new Leetcode376();
+        System.out.println(leetcode.wiggleMaxLength(new int[]{3,3,3,2,5}));
+
+    }
 }
