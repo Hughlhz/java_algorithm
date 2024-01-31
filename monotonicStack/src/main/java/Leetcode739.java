44355642d0480c08cf7517286d2e4824379fabb9
Hughlhz <hughes.lhz@gmail.com>
2024-01-31 15:55:00 +0800

    单调栈1

--- monotonicStack/src/main/java/Leetcode739.java
+++ monotonicStack/src/main/java/Leetcode739.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Leetcode739 {
+import java.util.Stack;
+
+public class Leetcode739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        if (temperatures.length == 1) {
+            return new int[1];
+        }
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+        stack.push(0);
+        for (int i = 1; i < temperatures.length; i++) {
+            if (temperatures[i] <= temperatures[stack.peek()]) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty()&&temperatures[i] > temperatures[stack.peek()]) {
+                    result[stack.peek()] = i - stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        }
+        return result;
+    }
 }
