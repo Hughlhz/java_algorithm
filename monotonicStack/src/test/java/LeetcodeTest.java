48f3b755eb3cdbc62a47bf5b04f17223e26f85f7
Hughlhz <hughes.lhz@gmail.com>
2024-01-31 15:55:00 +0800

    单调栈1

--- monotonicStack/src/test/java/LeetcodeTest.java
+++ monotonicStack/src/test/java/LeetcodeTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class LeetcodeTest {
+import org.junit.Test;
+
+public class LeetcodeTest {
+    @Test
+
+
+    public void leetcode739Test() {
+        Leetcode739 leetcode = new Leetcode739();
+        int nums[] = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        leetcode.dailyTemperatures(nums);
+
+    }
+
 }
