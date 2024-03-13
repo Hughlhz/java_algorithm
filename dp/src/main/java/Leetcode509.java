1fd2941a6ce60cf4eff562e34a31f4c97662e644
Hughlhz <1584926351@qq.com>
2024-03-13 20:39:14 +0800

    新增了动态规划（dp）问题的题解

---
+++ dp/src/main/java/Leetcode509.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode509 {
+    public int Fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
---
+++ dp/src/test/java/LeetcodeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class LeetcodeTest {
+    @Test
+    public void leetcode509Test() {
+        Leetcode509 leetcode509 = new Leetcode509();
+
+        System.out.println(leetcode509.Fib(4));
+    }
+
+}
