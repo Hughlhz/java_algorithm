11cad7ba429462ba49a8153030e35d61b6c8cd9c
Hughlhz <1584926351@qq.com>
2024-04-18 11:51:18 +0800

    增加sql练习的题解

--- sql/src/main/java/LeetcodeSql.java
+++ sql/src/main/java/LeetcodeSql.java
@@ -39,9 +39,29 @@ public class LeetcodeSql {
     public void sql1068() {
         //左连接
     }
+
     @Select("select a.customer_id, count(a.customer_id)as count_no_trans from Visits a left join Transactions b on a.visit_id =b.visit_id where transaction_id is null group by a.customer_id ;")
     public void sql1581() {
         //左连接+group by +聚类函数
     }
 
+    @Select("select a.id from Weather a  join Weather b on datediff(a.recordDate,b.recordDate)=1 and a.Temperature>b.Temperature;")
+    public void sql197() {
+        //考察自连接，和普通连接没有区别，连接时条件用datediff() and xxxx;
+    }
+
+    @Select("select a.machine_id as machine_id, round(avg(b.timestamp-a.timestamp),3) as processing_time from Activity a join Activity b on a.machine_id=b.machine_id and a.process_id= b.process_id and a.activity_type=\"start\" and b.activity_type=\"end\" group by a.machine_id;")
+    public void sql1661() {
+        //考察自连接和group by 、聚类函数以及round方法
+    }
+
+    @Select("select e.name,b.bonus from Employee e left join Bonus b on e.empId=b.empId where ifnull(b.bonus,0)<1000;")
+    public void sql577() {
+        //普通左连接
+    }
+
+    @Select("select A.student_id,A.student_name,A.subject_name,ifnull(B.attended_exams,0) attended_exams from (select * from Subjects, Students) A left join (select *,count(student_id) as attended_exams from Examinations group by student_id,subject_name) B on A.student_id=B.student_id and A.subject_name=B.subject_name order by A.student_id,A.subject_name;")
+    public void sql1280() {
+        //笛卡尔积和full join
+    }
 }
