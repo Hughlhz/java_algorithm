3a158cb81d3c76954109964695137e86a1eafb8e
Hughlhz <1584926351@qq.com>
2024-05-31 18:49:11 +0800

    多线程相关demo

---
+++ thread/src/main/java/CollectionsTest/HashTableDemo.java
@@ -0,0 +1,9 @@
+package CollectionsTest;
+
+import java.util.Hashtable;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        new Hashtable<>();
+    }
+}
---
+++ thread/src/main/java/Thread01/ConditionDemo.java
@@ -0,0 +1,63 @@
+package Thread01;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionDemo {
+    //     final Object a = new Object();
+//     Object b = new Object();
+//    Semaphore a = new Semaphore(1);
+//    Semaphore b = new Semaphore(0);
+    Lock lock=new ReentrantLock();
+    Condition a =lock.newCondition();
+    Condition b =lock.newCondition();
+    int state=0;
+
+    public  void printA() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            lock.lock();
+            while (state!=0){
+                a.await();
+            }
+            System.out.println("A");
+            state=1;
+            b.signal();
+            lock.unlock();
+        }
+    }
+    public  void printB() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            lock.lock();
+            while (state!=1){
+                b.await();
+            }
+            System.out.println("B");
+            state=0;
+            a.signal();
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ConditionDemo waitDemo = new ConditionDemo();
+        Thread a=new Thread(() -> {
+            try {
+                waitDemo.printA();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread b=new Thread(() -> {
+            try {
+                waitDemo.printB();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        a.start();
+        b.start();
+        a.join();
+        b.join();
+    }
+}
